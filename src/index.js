3ec732b1bc32c2162e76e47e533e1fb605331289
peter caufield <caufield.peterj@gmail.com>
2021-05-03 12:09:32 -0400

    cleaning up components

--- src/index.js
+++ src/index.js
@@ -7,43 +7,20 @@ function Booklist() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   )
 }
 
+const title = "A Tale of Two Cities"
+const author = "Charles Dickens"
 const Book = () => {
   return (
     <article className='book'>
-      <Image/>
-      <Title/>
-      <Author/>  
+      <img className="image" src="https://litexplore.files.wordpress.com/2014/05/a-tale-of-two-cities.jpg"/>
+      <h3 className='booktitle'>{title}</h3>
+      <h3>{author}</h3>
     </article>
   )
 }
-
-const Image = () => {
-  return (
-    <img className="image" src="https://litexplore.files.wordpress.com/2014/05/a-tale-of-two-cities.jpg"/>
-  )
-}
-
-const Title = () => {
-  return ( 
-    <h3 className='booktitle'>A Tale of Two Cities</h3>
-  )
-}
-
-const Author = () => {
-  return (
-    <h3>Charles Dickens</h3>
-  )
-}
-
-
 
 ReactDom.render(<Booklist />, document.getElementById('root'))
