406638b2046f4c89f0e19f1fe98ec255da9cb00c
peter caufield <caufield.peterj@gmail.com>
2021-05-03 12:14:18 -0400

    moving JS variables outside of component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// import CSS
 import './index.css'
+
+// setup variables
+const title = "A Tale of Two Cities"
+const author = "Charles Dickens"
+const img = "https://litexplore.files.wordpress.com/2014/05/a-tale-of-two-cities.jpg"
 
 function Booklist() {
   return (
@@ -11,14 +17,12 @@ function Booklist() {
   )
 }
 
-const title = "A Tale of Two Cities"
-const author = "Charles Dickens"
 const Book = () => {
   return (
     <article className='book'>
-      <img className="image" src="https://litexplore.files.wordpress.com/2014/05/a-tale-of-two-cities.jpg"/>
+      <img className="image" src={img}/>
       <h3 className='booktitle'>{title}</h3>
-      <h3>{author}</h3>
+      <h3>{author.toUpperCase()}</h3>
     </article>
   )
 }
