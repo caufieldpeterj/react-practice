232719b960a97de96144a283b968fa3a02c0dc67
peter caufield <caufield.peterj@gmail.com>
2021-05-03 17:07:09 -0400

    iterating over book list using map, dynamically creating components

--- src/index.js
+++ src/index.js
@@ -5,40 +5,46 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 // setup variables
-const firstBook = {
+const books = [
+{
+  id: 1,
   img: "https://litexplore.files.wordpress.com/2014/05/a-tale-of-two-cities.jpg",
   title: "A Tale of Two Cities",
   author: "Charles Dickens"
-}
-
-const secondBook = {
+},
+{
+  id: 2,
   img: "https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg",
   title: "LOTR",
   author: "JRR Tolkien"
-}
+},
+{
+  id: 3,
+  img: "https://cdn.shopify.com/s/files/1/0346/6845/products/Born-To-Run-Product-book-cover_1200x.jpg?v=1568676021",
+  title: "Born to Run",
+  author: "Christopher McDougall"
+}]
+
+
 
 function Booklist() {
   return (
     <section className='booklist'>
-      {/* when rendering the book list we pass in props name:'value'  */}
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-        <p>
-          Sit ut dolore aute quis sint et ut veniam dolore ullamco proident eu ullamco sit. Deserunt voluptate velit laboris dolor proident id esse eu reprehenderit est sint cillum Lorem. Ipsum dolore occaecat ex fugiat amet dolor esse magna eu reprehenderit. Deserunt esse ut duis consectetur labore. Labore exercitation et nostrud excepteur.
-        </p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+      {books.map((book) => {
+        return (
+          <Book key={book.id} book={book}></Book>
+        )
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  console.log(props);
   return (
     <article className='book'>
-      <img className="image" src={props.img} alt="book cover"/>
-      <h3 className='booktitle'>{props.title}</h3>
-      <h3>{props.author}</h3>
-      {props.children}
+      <img className="image" src={props.book.img} alt="book cover"/>
+      <h3 className='booktitle'>{props.book.title}</h3>
+      <h3>{props.book.author}</h3>
     </article>
   )
 }
