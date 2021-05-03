632984c0bdad2ea633e8f7869b9bc574f409ceaf
peter caufield <caufield.peterj@gmail.com>
2021-05-03 17:31:33 -0400

    final push before react hooks

--- src/Book.js
+++ src/Book.js
@@ -4,14 +4,14 @@ const Book = (props) => {
     // attribute and eventHandler
     // onClick, onMouseover
     const clickHandler = () => {
-      alert(props.title);
+      alert(`Are you sure you want to buy ${props.title}`);
     }
     return (
       <article className='book'>
         <img className="image" src={props.img} alt="book cover"/>
         <h3 className='booktitle'>{props.title}</h3>
         <h3>{props.author}</h3>
-        <button type="button" onClick={clickHandler}>Hello World</button>
+        <button type="button" onClick={clickHandler}>Purchase Book</button>
       </article>
     )
 }
